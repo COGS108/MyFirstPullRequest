07f79a5c67717bf60dd47d946731d786f30df7a0
Cyuaz <79662806+Cyuaz@users.noreply.github.com>
2021-11-03 23:34:18 -0700

    Add files via upload

---
+++ barrel_shifter.sv
@@ -0,0 +1,16 @@
+// Barrel Shifter RTL Model
+`include "mux_2x1_behavioral.sv"
+module barrel_shifter (
+  input logic select,  // select=0 shift operation, select=1 rotate operation
+  input logic direction, // direction=0 right move, direction=1 left move
+  input logic[1:0] shift_value, // number of bits to be shifted (0, 1, 2 or 3)
+  input logic[3:0] din,
+  output logic[3:0] dout
+);
+
+// Students to add code for barrel shifter
+
+
+endmodule: barrel_shifter
+
+
