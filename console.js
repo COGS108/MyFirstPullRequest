e0229f3cf045d81ea06203df9680a195c4af5da7
Thant Zin Phyo <109084377+ThantZinPhyo-SyS@users.noreply.github.com>
2023-07-05 17:18:58 +0630

    Update console.js

--- console.js
+++ console.js
@@ -1,3 +1,9 @@
 // this is a commit
 console. log("Hello World")
-console. log("Hello World")
+
+var greetingStart = "Hello, my name is ";
+var name = "Daniel";
+
+var greeting = greetingStart + name;
+
+console.log(greeting); // Logs "Hello, my name is Daniel"
